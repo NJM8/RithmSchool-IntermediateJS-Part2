39c4761db18ec6752352e0dde8b53b6c91098c65
Nate Mallison <healthynate@gmail.com>
2017-10-19 06:18:25 -0400

    completed some reduce exercises

--- iterators.js
+++ iterators.js
@@ -76,7 +76,42 @@ console.log(filterKey([{name: "Elie", isInstructor:true, isHilarious: false},{na
 );
 
 
+// Using reduce Write a function called extractKey which accepts two parameters, an array of objects, and the name of a key and returns an array with just the values for that key:
 
+function reduceExtractKey(array, key){
+	return array.reduce(function(acc, next){
+		acc.push(next[key]);
+		return acc;
+	}, []);
+}
+
+console.log(reduceExtractKey([{name: "Elie", isInstructor:true},{name: "Tim", isInstructor:true},{name: "Matt", isInstructor:true}], "name"));
+
+// Using reduce Write a function called filterLetters which accepts an array of letters and returns the number of occurrences of a specific letter. This function should be case insensitive
+
+function reduceFilterLetters(array, letter){
+	return array.reduce(function(acc, next){
+		if (next.toLowerCase() === letter.toLowerCase()) {
+			acc += 1;
+		}
+		return acc;
+	}, 0);
+}
+
+console.log(reduceFilterLetters(["a","a","b","c","A"], "a")); // 3
+console.log(reduceFilterLetters(["a","a","b","c","A"], "z")); // 0
+console.log(reduceFilterLetters(["a","a","b","c","A"], "B")); // 1
+
+// Using reduce Write a function called addKeyAndValue which accepts three parameters, an array (of objects), a key and a value. This function should return the array of objects after each key and value has been added. You can do this a few ways, either by reducing starting with an empty array and making copies of the object or by starting with the actual array!
+
+function reduceAddKeyAndValue(array, key, value){
+	return array.reduce(function(acc, next, index){
+		acc[index][key] = value;
+		return acc;
+	}, array);
+}
+
+console.log(reduceAddKeyAndValue([{name: 'Elie'},{name: 'Tim'},{name: 'Elie'}], "isInstructor", true));
 
 
 
