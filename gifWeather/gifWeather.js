206b4be9eedcea7663c7fc7e4b3f05fe5b6aefa5
Nate Mallison <healthynate@gmail.com>
2017-12-11 21:40:21 -0500

    all weather gifs working

--- gifWeather/gifWeather.js
+++ gifWeather/gifWeather.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	})
 
 	$('#check').on('click', function(){
-		let location = $('#query').val().split(' ');
+		let location = $('#query').val();
 
 		if (location === '') {
 			alert("please enter something in the search field.");
@@ -19,7 +19,7 @@ $(document).ready(function(){
 
 		// first get lat and long
 
-		$.when($.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + location[0] + location[1] + '&key=AIzaSyAa2Fm1SPYpC2qkif3suYQyLYGtBthsYtQ'))
+		$.when($.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + location[0] + '&key=AIzaSyAa2Fm1SPYpC2qkif3suYQyLYGtBthsYtQ'))
 			.then(function(response){
 				let latLng = [];
 				latLng.push(response.results[0].geometry.location.lat);
@@ -30,12 +30,76 @@ $(document).ready(function(){
 				$.when($.get("https://fcc-weather-api.glitch.me/api/current?lat=" + latLng[0] + "&lon=" + latLng[1]))
 					.then(function(response){
 						let weather = response.weather[0].description;
-						let tempurature = response.main.temp;
+						let tempurature = (response.main.temp * 1.8) + 32;
 						let wind = response.wind.speed;
+						let tempuratureDescription = '';
+						let windDescription = '';
+
+						switch (true) {
+							case tempurature < 32:
+								tempuratureDescription = 'freezing';
+								break;
+							case 32 < tempurature && tempurature < 60:
+								tempuratureDescription = 'cold';
+								break;
+							case 60 < tempurature && tempurature < 80:
+								tempuratureDescription = 'comfortable';
+								break;
+							case 80 < tempurature && tempurature < 100:
+								tempuratureDescription = 'hot';
+								break;
+							case 100 < tempurature:
+								tempuratureDescription = 'sweltering';
+								break;
+						}
+
+						switch (true) {
+							case wind < 4:
+								windDescription = 'mild';
+								break;
+							case 4 < wind && wind < 8:
+								windDescription = 'breezy';
+								break;
+							case 8 < wind && wind < 12:
+								windDescription = 'windy';
+								break;
+							case 12 < wind && wind < 16:
+								windDescription = 'blustery';
+								break;
+							case 16 < wind:
+								windDescription = 'gusty';
+								break;
+						}
 
 						// then find gifs
 
 						$.when($.get("http://api.giphy.com/v1/gifs/search?q=" + weather + "&api_key=fLBc8pLLd3UMGWIvHv1hRu2tlwKbGBvE&limit=5"))
+							.then(function(data){
+								var randNum = Math.floor(Math.random() * 5);
+								var $newGif = $("<iframe>", {
+									src: data.data[randNum].embed_url, 
+									width: '300', 
+									height: '300', 
+									class: 'giphy-embed', 
+									frameBorder: '0', 
+								});
+								$('#gifs').append($newGif);
+							});
+
+						$.when($.get("http://api.giphy.com/v1/gifs/search?q=" + tempuratureDescription + "&api_key=fLBc8pLLd3UMGWIvHv1hRu2tlwKbGBvE&limit=5"))
+							.then(function(data){
+								var randNum = Math.floor(Math.random() * 5);
+								var $newGif = $("<iframe>", {
+									src: data.data[randNum].embed_url, 
+									width: '300', 
+									height: '300', 
+									class: 'giphy-embed', 
+									frameBorder: '0'
+								});
+								$('#gifs').append($newGif);
+							});
+
+						$.when($.get("http://api.giphy.com/v1/gifs/search?q=" + windDescription + "&api_key=fLBc8pLLd3UMGWIvHv1hRu2tlwKbGBvE&limit=5"))
 							.then(function(data){
 								var randNum = Math.floor(Math.random() * 5);
 								var $newGif = $("<iframe>", {
@@ -55,4 +119,5 @@ $(document).ready(function(){
 	$('#delete').on('click', function(){
 		$('.giphy-embed').remove();
 	})
+
 })
